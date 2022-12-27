30bcd511b2e0515d12a7f13f85533712af891d95
Fritzi Fritte <frans.schotsch@gmx.de>
2022-12-27 12:36:45 +0100

    changed static root

--- sonne/sonne/settings.py
+++ sonne/sonne/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = '/home/frotz/frotz.pythonanywhere.com/sonne/adressen/static/'
 STATIC_URL = '/home/frotz/frotz.pythonanywhere.com/sonne/adressen/static/'
 
 # Default primary key field type
