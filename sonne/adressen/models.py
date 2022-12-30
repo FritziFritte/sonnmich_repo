bae9a1ea02f7d06a9da1d7b5d2b15c7e784b756c
Fritzi Fritte <frans.schotsch@gmx.de>
2022-12-30 13:01:02 +0100

    file test

--- sonne/adressen/models.py
+++ sonne/adressen/models.py
@@ -8,12 +8,11 @@ class Adressen(models.Model):
     hausnummer = models.IntegerField()
     plz = models.IntegerField()
     ort = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/', max_length=255)
-
+    image = models.ImageField(upload_to='images/')
     
     
     def __str__(self): # Neu
-        return self.nachname # Neu
+        return self.name # Neu
 
     def get_absolute_url(self): # Neu
         return reverse('adress_detail', args=[str(self.id)]) # Neu
