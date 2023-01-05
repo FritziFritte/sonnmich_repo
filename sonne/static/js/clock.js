8729e218fb080c2a7abd10a209f99c7935c8be5e
Fritzi Fritte <frans.schotsch@gmx.de>
2023-01-05 12:52:59 +0100

    qwe

--- sonne/static/js/clock.js
+++ sonne/static/js/clock.js
@@ -6,9 +6,24 @@ function display_c(){
 function display_ct() {
   var images = document.querySelectorAll("img");
   for (var i = 0; i < images.length; i++) {
-    var x = new Date();
     var pk = images[i].getAttribute("data-pk");
-    document.getElementById('ct_' + pk).innerHTML = x.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+
+    var currentTime = new Date();
+    var targetTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), 14, 0, 0);
+  
+    var timeDifference = currentTime - targetTime; 
+    var hoursDifference = Math.floor(timeDifference / 1000 / 60 / 60);
+    var minutesDifference = Math.floor((timeDifference / 1000 / 60) - (hoursDifference * 60));
+
+    //if (timeDifference <= 0) {
+    //  document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' +  Math.abs(hoursDifference) + ':' + Math.abs(minutesDifference);
+    //} else {
+    //  document.getElementById('ct_' + pk).innerHTML = 'Noch ' + Math.abs(hoursDifference) + ':' + Math.abs(minutesDifference) + ' Sonne';
+    //}
+    //document.getElementById('ct_' + pk).innerHTML = "seconds " + timeDifference_seconds + " " + "minutes " + timeDifference_minutes;
+
+    document.getElementById('ct_' + pk).innerHTML = hoursDifference + ':' + minutesDifference;
+    
     
   }
 
