fd436000eb2d535bac9ae1f92a0f5ae413480d3b
Fritzi Fritte <frans.schotsch@gmx.de>
2023-02-05 13:12:43 +0100

    asd

--- sonne/static/js/clock.js
+++ sonne/static/js/clock.js
@@ -10,22 +10,52 @@ function display_ct() {
 
     var currentTime = new Date();
     var targetTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), 14, 0, 0);
+    var stopTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), 16, 0, 0);
   
     var timeDifference = currentTime - targetTime; 
-    var hoursDifference = Math.floor(timeDifference / 1000 / 60 / 60);
-    var minutesDifference = Math.floor((timeDifference / 1000 / 60) - (hoursDifference * 60));
+    var hoursDifference = Math.floor(Math.abs(timeDifference / 1000 / 60 / 60));
+    var minutesDifference = Math.floor(Math.abs(timeDifference / 1000 / 60) - (hoursDifference * 60));
 
-    //if (timeDifference <= 0) {
-    //  document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' +  Math.abs(hoursDifference) + ':' + Math.abs(minutesDifference);
-    //} else {
-    //  document.getElementById('ct_' + pk).innerHTML = 'Noch ' + Math.abs(hoursDifference) + ':' + Math.abs(minutesDifference) + ' Sonne';
-    //}
+
+
+    if (timeDifference <= 0) {
+      if (hoursDifference<10) {
+        if (minutesDifference<10) {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + '0' + hoursDifference + ':' + '0' + minutesDifference;
+        } else {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + '0' + hoursDifference + ':' + minutesDifference;
+        }
+      } else {
+        if (minutesDifference<10) {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + hoursDifference + ':' + '0' + minutesDifference;
+        } else {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + hoursDifference + ':' + minutesDifference;
+        }
+      }
+    } else {
+      if (hoursDifference<10) {
+        if (minutesDifference<10) {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + '0' + hoursDifference + ':' + '0' + minutesDifference;
+        } else {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + '0' + hoursDifference + ':' + minutesDifference;
+        }
+      } else {
+        if (minutesDifference<10) {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + hoursDifference + ':' + '0' + minutesDifference;
+        } else {
+          document.getElementById('ct_' + pk).innerHTML = 'Sonne in ' + hoursDifference + ':' + minutesDifference;
+        }
+      }
+    }
+  }
+
+
+
+
     //document.getElementById('ct_' + pk).innerHTML = "seconds " + timeDifference_seconds + " " + "minutes " + timeDifference_minutes;
 
-    document.getElementById('ct_' + pk).innerHTML = hoursDifference + ':' + minutesDifference;
-    
-    
-  }
+    //document.getElementById('ct_' + pk).innerHTML = timeDifference;
+  
 
   display_c();
 }
